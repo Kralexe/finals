3b5cec3a741200b08413e2b3bea8f823e675e5ec
La <rer@ya.ru>
2019-12-10 00:08:00 +0300

    first commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Web;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+using System.Xml;
+using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.res = TempData["res"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Upload()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormFile file, string text, string typee, string mes)
+        {
+            if (mes==null)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                string tex = string.Empty;
+                var ext = Path.GetExtension(fileName).ToLowerInvariant();
+                if (ext == ".docx" || ext == ".txt")
+                    if (ext == ".docx")
+                    {
+                        using (var localFile = System.IO.File.OpenWrite("ff.docx"))
+                        using (var uploadedFile = file.OpenReadStream())
+                        {
+                            uploadedFile.CopyTo(localFile);
+                        }
+                        using (var wordyDocument = WordprocessingDocument.Open("ff.docx", false))
+                        {
+                            tex = wordyDocument.MainDocumentPart.Document.Body.InnerText;
+                        }
+                        System.IO.File.Delete("ff.docx");
+                    }
+                    else
+                    {
+                        using (var localFile = System.IO.File.OpenWrite("ff.txt"))
+                        using (var uploadedFile = file.OpenReadStream())
+                        {
+                            uploadedFile.CopyTo(localFile);
+                        }
+                        tex = System.IO.File.ReadAllText("ff.txt");
+                        System.IO.File.Delete("ff.txt");
+                    }
+                if (typee.Contains("ye"))
+                {
+                    TempData["res"] = FuncSupply.Decrypt(tex, text);
+                }
+                else
+                {
+                    TempData["res"] = FuncSupply.Encrypt(tex, text);
+                }
+            }
+            else
+            {
+                if (typee.Contains("ye"))
+                {
+                    TempData["res"] = FuncSupply.Decrypt(mes, text);
+                }
+                else
+                {
+                    TempData["res"] = FuncSupply.Encrypt(mes, text);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult DownloadTxt(string result)
+        {
+            if (result!=null)
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(result);
+                MemoryStream stream = new MemoryStream(byteArray);
+                return File(stream, FuncSupply.GetContentType("fff.txt"), Path.GetFileName("Result.txt"));
+            }
+            else return NoContent();
+        }
+        public IActionResult DownloadDocx(string result)
+        {
+            if (result!=null)
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(result);
+                using (WordprocessingDocument wordDocument =
+                WordprocessingDocument.Create(Path.GetTempPath() + "ffff.docx", WordprocessingDocumentType.Document))
+                {
+                    // Add a main document part. 
+                    MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                    // Create the document structure and add some text.
+                    mainPart.Document = new Document();
+                    Body body = mainPart.Document.AppendChild(new Body());
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+                    run.AppendChild(new Text(result));
+                }
+                return PhysicalFile(Path.GetTempPath() + "ffff.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "ffds.docx");
+            }
+            else return NoContent();
+        }
+
+    }
+}
---
+++ WebApp/FuncSupply.cs
@@ -0,0 +1,91 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    class FuncSupply
+    {
+        static char[] characters = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+                                                'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+                                                'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
+                                                'э', 'ю', 'я' };
+        static int N = characters.Length;
+        public static string Encrypt(string input, string keyword)
+        {
+            char[] m = input.ToLower().ToCharArray();
+            char[] k = keyword.ToLower().ToCharArray();
+
+            int keyword_index = 0;
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (characters.Contains(m[i]))
+                {
+                    int p = (Array.IndexOf(characters, m[i]) +
+                        Array.IndexOf(characters, k[keyword_index])) % N;
+
+                    m[i] = characters[p];
+
+                    keyword_index++;
+
+                    if ((keyword_index + 0) == keyword.Length)
+                        keyword_index = 0;
+                }
+            }
+
+            return new string(m);
+        }
+        public static string Decrypt(string input, string keyword)
+        {
+            char[] m = input.ToLower().ToCharArray();
+            char[] k = keyword.ToLower().ToCharArray();
+            int keyword_index = 0;
+
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (characters.Contains(m[i]))
+                {
+                    int p = (Array.IndexOf(characters, m[i]) + N -
+                        Array.IndexOf(characters, k[keyword_index])) % N;
+
+                    m[i] = characters[p];
+
+                    keyword_index++;
+
+                    if ((keyword_index + 0) == keyword.Length)
+                        keyword_index = 0;
+                }
+            }
+            return new string(m);
+        }
+        public static string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+        
+        public static Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
+        }
+    }
+}
